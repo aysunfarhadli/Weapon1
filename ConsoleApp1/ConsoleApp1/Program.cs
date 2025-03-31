11edd154392774a3fbeda926710cb52485fc90ba
AbidovaGuler <gullarabidova@gmail.com>
2025-03-31 13:25:22 +0400

    weapon

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int tutum, gulle;
+            bool mod;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Daraq tutumunu daxil et: ");
+                    tutum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Daraqdaki gulle sayini daxil et: ");
+                    gulle = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Atis modu (1 - avtomatik, 0 - tekli): ");
+                    mod = Console.ReadLine() == "1";
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Yanlis daxil etdiniz, yeniden cehd edin.");
+                }
+            }
+
+            Weapon weapon = new Weapon(tutum, gulle, mod);
+
+            while (true)
+            {
+                Console.Write("\n0-Info, 1-Shoot, 2-GetRemainBulletCount, 3-Reload, 4-ChangeFireMode, 5-Exit: ");
+                string secim = Console.ReadLine();
+
+                if (secim == "0")
+                {
+                    Console.WriteLine("0 - İnformasiya almaq üçün\n1 - Shoot metodu üçün\n2 - GetRemainBulletCount metodu üçün\n3 - Reload metodu üçün\n4 - ChangeFireMode metodu üçün\n5 - Proqramdan çıxmaq üçün");
+                }
+                else if (secim == "1")
+                {
+                    weapon.Shoot();
+                }
+                else if (secim == "2")
+                {
+                    Console.WriteLine("Tam dolmasi ucun lazim olan gulle: " + weapon.GetRemainBulletCount());
+                }
+                else if (secim == "3")
+                {
+                    weapon.Reload();
+                }
+                else if (secim == "4")
+                {
+                    weapon.ChangeFireMode();
+                }
+                else if (secim == "5")
+                {
+                    Console.WriteLine("Proqram dayandirildi.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Yanlis secim, yeniden cehd edin.");
+                }
+            }
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/ConsoleApp1/weapon.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Weapon
+    {
+        int daraqTutumu;
+        int gulleSayi;
+        bool avtomatikAtis;
+
+        public Weapon(int tutum, int gulle, bool mod)
+        {
+            if (tutum <= 0)
+            {
+                tutum = 10;
+                Console.WriteLine("Daraq tutumu sehv daxil edildi, 10 olaraq teyin edildi.");
+            }
+
+            if (gulle < 0 || gulle > tutum)
+            {
+                gulle = tutum;
+                Console.WriteLine("Gulle sayi sehv daxil edildi, daraq tam dolduruldu.");
+            }
+
+            daraqTutumu = tutum;
+            gulleSayi = gulle;
+            avtomatikAtis = mod;
+        }
+
+        public void Shoot()
+        {
+            if (gulleSayi == 0)
+            {
+                Console.WriteLine("Daraq bosdur!");
+                return;
+            }
+
+            if (avtomatikAtis)
+            {
+                Console.WriteLine(gulleSayi + " gulle atildi!");
+                gulleSayi = 0;
+            }
+            else
+            {
+                Console.WriteLine("1 gulle atildi!");
+                gulleSayi--;
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return daraqTutumu - gulleSayi;
+        }
+
+        public void Reload()
+        {
+            gulleSayi = daraqTutumu;
+            Console.WriteLine("Daraq tam dolduruldu! Gulle sayi: " + gulleSayi);
+        }
+
+        public void ChangeFireMode()
+        {
+            avtomatikAtis = !avtomatikAtis;
+            Console.WriteLine(avtomatikAtis ? "Atis modu: Avtomatik" : "Atis modu: Tekli");
+        }
+    }
+
+}
